0487f1983fc2d2824f3239ad316732d3bdc02172
Chidambaram Ramanathan <chidambaram.ramanath@gmail.com>
2014-03-19 19:44:26 -0400

    Modified master script and added script for min config

---
+++ script/kconfig.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+	var=`grep -v \# ./../../kernel.config|grep \= |wc -l`
+	echo "Total number of configuration lines except comments = "$var
+	var=`ls -lh /boot/vmlinuz-3.2.2+`
+	echo "Total size of the binary = "$var
--- script/master_script.sh
+++ script/master_script.sh
@@ -17,41 +17,82 @@
 #	Then when the master script is executed again, give "n" so that it will start executing all the sub scripts
 ###
 
-jump () {
-	cd ./../
-	make clean &
-	wait
-	make &
-	wait
-	var=`grep -v \# kernel.config|grep \= |wc -l`
-	echo "Total number of configuration lines except comments = "$var
-	var=`ls -lh /boot/vmlinuz-3.2.2+`
-	echo "Total size of the binary = "$var
-}
-
-echo "HI!!! Welcome. Do you want to perform make?"
-read abc
-
-if [ "$abc" = "y" ]; then
-	jump
-else
 	# If you come here, it means the system has booted with the students' kernel
+	echo "Enter the student's name: "
+	read name
+	mkdir /home/chid/$name
+	
+	echo "$(tput setaf 3)"
+	echo "===== Writing the kernel config details ====="
+	echo "$(tput sgr0)"
+	sh kconfig.sh 2>&1 | tee /home/chid/$name/kconfig.txt
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 1 ====="
+	echo "$(tput sgr0)"
+	sh testcase1_SmallOrEmptyFiles.sh 2>&1 | tee /home/chid/$name/testcase1.txt	
+	printf "\n"
+
 	echo "$(tput setaf 3)"
 	echo "===== TEST CASE 2 ====="
 	echo "$(tput sgr0)"
-	sh ./testcase_2_largeFiles.sh
+	sh testcase2_LargeFiles.sh 2>&1 | tee /home/chid/$name/testcase2.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 6.1 ====="
+	echo "$(tput sgr0)"
+	sh testcase6_1_APPEND.sh 2>&1 | tee /home/chid/$name/testcase6_1.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 6.2 ====="
+	echo "$(tput sgr0)"
+	sh testcase6_2_TRUNC.sh 2>&1 | tee /home/chid/$name/testcase6_2.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 6.3 ====="
+	echo "$(tput sgr0)"
+	sh testcase6_3_EXCL.sh 2>&1 | tee /home/chid/$name/testcase6_3.txt
 	printf "\n"
 
 	echo "$(tput setaf 3)"
 	echo "===== TEST CASE 7 ====="
 	echo "$(tput sgr0)"
-	sh ./testcase_7_appropriateErrno.sh
+	sh testcase7_appropriateErrno.sh 2>&1 | tee /home/chid/$name/testcase7.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 8 & 9 ====="
+	echo "$(tput sgr0)"
+	sh testcase8_9_InvalidAddress.sh 2>&1 | tee /home/chid/$name/testcase8_9.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 10 ====="
+	echo "$(tput sgr0)"
+	sh testcase10_appropriateErrMessage.sh 2>&1 | tee /home/chid/$name/testcase10.txt
+	printf "\n"
+
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 11 ====="
+	echo "$(tput sgr0)"
+	sh testcase11_incorrectArguments.sh 2>&1 | tee /home/chid/$name/testcase11.txt
 	printf "\n"
 
 	echo "$(tput setaf 3)"
 	echo "===== TEST CASE 12 ====="
 	echo "$(tput sgr0)"
-	sh ./testcase_12_invalidFlags.sh
+	sh testcase12_invalidFlags.sh 2>&1 | tee /home/chid/$name/testcase12.txt
 	printf "\n"
 
-fi
+	echo "$(tput setaf 3)"
+	echo "===== TEST CASE 13 ====="
+	echo "$(tput sgr0)"
+	sh testcaseMemLeak.sh 2>&1 | tee /home/chid/$name/testcaseMemLeak.txt
+	printf "\n"
+
+	rm -f /home/chid/output/out*
+	rm -f /home/chid/error/error3
+
